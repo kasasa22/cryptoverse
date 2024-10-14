42d872e695e589ec224ee78a1f18b2b16a4ffcb9
kasasa22 <kasasatrevor25@gmail.com>
2024-10-14 22:35:55 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React from 'react'
-import { Switch, Route , Link } from 'react-router-dom'
-import { Layout, Typography, Space } from 'antd'
-import Navbar from './components/Navbar'
-import './app.css'
+import React from 'react';
+import { Routes, Route} from 'react-router-dom';
+import { Layout, Typography} from 'antd';
+import { Navbar, Homepage, Exchanges, CryptoDetails, Cryptocurrencies, News } from './components';
+import './app.css';
 
 const App = () => {
   return (
-   <div className='app'>
-    <div className='navbar'>
-      <Navbar/> 
-
+    <div className='app'>
+      <div className='navbar'>
+        <Navbar />
+      </div>
+      <div className='main'>
+        <Layout>
+          <div className='routes'>
+            <Routes>
+              <Route path='/' element={<Homepage />} />
+              <Route path='/exchanges' element={<Exchanges />} />
+              <Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
+              <Route path='/crypto/:coinid' element={<CryptoDetails />} />
+              <Route path='/news' element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+      </div>
+      <div className='footer' level={5} >
+        <Typography.Title style={{ color:'white' , textAlign:'center'}}>
+          Cryptoverse <br/>
+          all rights reserved
+        </Typography.Title>
+      </div>
     </div>
-    <div className='main'>
+  );
+};
 
-    </div>
-    <div className='footer'>
-
-    </div>
-
-   </div>
-  )
-}
-
-export default App
+export default App;
